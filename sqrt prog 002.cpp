36230ddcaf72c7882db1af5d097a0de9ca1c1448
khaakila <98585432+khaakila@users.noreply.github.com>
2022-05-23 11:41:35 +0300

    Add files via upload

---
+++ sqrt prog 002.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<math.h>
+using namespace std;
+
+int main()
+{
+float sq,n;
+cout<<"enter any number:";
+cin>>n;
+sq = sqrt (n);
+cout<< "square root of "<<n<<" is "<<sq;
+return 0;
+}

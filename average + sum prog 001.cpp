e8c66dc9350c45eec77ab7c6d69969e811d884bc
khaakila <98585432+khaakila@users.noreply.github.com>
2022-05-23 11:40:20 +0300

    Add files via upload

---
+++ average + sum prog 001.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+using namespace std;
+int main()
+
+{
+int a , b, sum;
+
+ float average;
+
+ cout<<("enter the first number:");
+ cin>>a;
+
+ cout<<("enter second number");
+ cin>>b;
+
+ sum = a + b;
+
+ average = sum/2;
+
+ cout<<"the sum of the two numbers is" <<sum<<end1;
+ cout<<"the average of the two numbers is" <<average<<end1;
+
+return 0;
+}

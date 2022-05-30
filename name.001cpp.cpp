257805746b5f1bf51da9984111cd01ed488d114f
khaakila <98585432+khaakila@users.noreply.github.com>
2022-05-30 12:18:25 +0300

    Update name.001cpp.cpp

--- name.001cpp.cpp
+++ name.001cpp.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 int main()
 {
-    string name;
-    cout<<("enter name");
-    cin>>name;
-    cout<<(name);
+     char first_name;[10]
+    cout<<"enter your fisrt name"<<;endl
+        
+    cin>> fisrt name;
+    
+    cout<< your fisrt name<<;
 
     return 0;
     }
